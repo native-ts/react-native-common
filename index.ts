10549beef1edc20f90b557d9ee94082d026c93fc
Cảnh Nguyễn Văn <ngvcanh2014@gmail.com>
2023-02-19 14:10:18 +0700

    UPDATE :: export global Point and Size

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 export * from './isEqual';
 export * from './hooks/use-window';
+export * from './types';
 export { default as isEqual } from './isEqual';
 export { default as useWindow } from './hooks/use-window';
+export { default as Point } from './Point';
